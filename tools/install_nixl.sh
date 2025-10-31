adf939ebe4dd7317f69ace3b9a81a8eb8590f066
apt-team <code.x.0018@gmail.com>
2025-10-31 07:04:55 +0400

    Add install_nixl_from_source_ubuntu.py to Dockerfile and update install_nixl.sh to reference it

--- tools/install_nixl.sh
+++ tools/install_nixl.sh
@@ -5,8 +5,9 @@
 
 set -e
 
+# In the Docker container, the Python script will be in the same directory as this script
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Run the Python installation script
+# Run the Python installation script from the same directory
 python3 "${SCRIPT_DIR}/install_nixl_from_source_ubuntu.py" "$@"
